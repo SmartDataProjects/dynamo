d3b36cdf96b7b683999b8b2d7fa7732ad0a66d3b
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-19 09:23:58 -0400

    allow 10 communication failures

--- lib/common/interface/htc.py
+++ lib/common/interface/htc.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import htcondor
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,11 @@ class HTCondor(object):
         schedd_ads = self._collector.query(htcondor.AdTypes.Schedd, schedd_constraint, ['ScheddIpAddr'])
         self._schedds = [htcondor.Schedd(ad) for ad in schedd_ads]
 
+        for schedd, ad in zip(self._schedds, schedd_ads):
+            matches = re.match('<([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):[0-9]+', ad['ScheddIpAddr'])
+            # schedd does not have an ipaddr attribute natively, but we can assign it
+            schedd.ipaddr = matches.group(1)
+
     def find_jobs(self, constraint = 'True', attributes = []):
         """
         Return ClassAds for jobs matching the constraints.
@@ -29,7 +35,18 @@ class HTCondor(object):
         classads = []
 
         for schedd in self._schedds:
-            classads += schedd.query(constraint, attributes)
+            attempt = 0
+            while True:
+                try:
+                    ads = schedd.query(constraint, attributes)
+                    break
+                except IOError:
+                    attempt += 1
+                    logger.info('IOError in communicating with schedd %s. Trying again.', schedd.ipaddr)
+                    if attempt == 10:
+                        raise
+                
+            classads += ads
 
         logger.info('HTCondor returned %d classads', len(classads))
 
