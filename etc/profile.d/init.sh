76d3f18c4b12830e36fa3645b08cb97b0d3c2e74
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-19 09:59:22 -0400

    dealer daemonized

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -1,4 +1,6 @@
 export DYNAMO_BASE=$(cd $(dirname $(dirname $(dirname ${BASH_SOURCE[0]}))); pwd)
 export DYNAMO_LOGDIR=/var/log/dynamo
+export DYNAMO_DETOX_INTERVAL=6
+export DYNAMO_DEALER_INTERVAL=1
 export PYTHONPATH=$DYNAMO_BASE/lib:$PYTHONPATH
-export DETOX_CYCLE_HOURS=6
+
