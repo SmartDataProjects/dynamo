5c99f64a20c41d151729cad9040bbdd41f964c0b
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-20 02:09:45 -0400

    multiple source safe

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -2,5 +2,4 @@ export DYNAMO_BASE=$(cd $(dirname $(dirname $(dirname ${BASH_SOURCE[0]}))); pwd)
 export DYNAMO_LOGDIR=/var/log/dynamo
 export DYNAMO_DETOX_INTERVAL=6
 export DYNAMO_DEALER_INTERVAL=1
-export PYTHONPATH=$DYNAMO_BASE/lib:$PYTHONPATH
-
+export PYTHONPATH=$DYNAMO_BASE/lib:$(echo $PYTHONPATH | sed "s|$DYNAMO_BASE/lib:||")
