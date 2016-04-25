8f535b67cd5e145b2f2bfe334f6b8e9033ba8f3c
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-25 18:42:35 -0400

    x509

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -2,4 +2,5 @@ export DYNAMO_BASE=$(cd $(dirname $(dirname $(dirname ${BASH_SOURCE[0]}))); pwd)
 export DYNAMO_LOGDIR=/var/log/dynamo
 export DYNAMO_DETOX_INTERVAL=6
 export DYNAMO_DEALER_INTERVAL=1
+export X509_USER_PROXY=/tmp/x509up_u$(uid -u)
 export PYTHONPATH=$DYNAMO_BASE/lib:$(echo $PYTHONPATH | sed "s|$DYNAMO_BASE/lib:||")
