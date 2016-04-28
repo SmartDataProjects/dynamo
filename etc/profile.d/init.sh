c52238123c01652ac67489107bb5214001c040df
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-28 11:36:20 -0400

    bugfix

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -1,4 +1,4 @@
-export DYNAMO_BASE=$(cd $(dirname $(dirname $(dirname ${BASH_SOURCE[0]}))); pwd)
+export DYNAMO_BASE=$(dirname $(dirname $(cd $(dirname ${BASH_SOURCE[0]}); pwd)))
 export DYNAMO_LOGDIR=/var/log/dynamo
 export DYNAMO_DETOX_INTERVAL=6
 export DYNAMO_DEALER_INTERVAL=1
