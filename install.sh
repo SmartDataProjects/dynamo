3db284b133448320a4b870303d1e9e5ee0d40212
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-28 15:08:00 -0400

    minor fixes

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 export DYNAMO_BASE=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 source $DYNAMO_BASE/etc/profile.d/init.sh
 
-sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/detoxd > /etc/init.d/
-sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dealerd > /etc/init.d/
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/detoxd > /etc/init.d/detoxd
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dealerd > /etc/init.d/dealerd
 
 mkdir -p $DYNAMO_LOGDIR
 chmod 775 $DYNAMO_LOGDIR
