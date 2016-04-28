8ebaa843d5e79a3302f120a707927770332d2005
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-28 15:20:05 -0400

    renaming daemons

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 export DYNAMO_BASE=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 source $DYNAMO_BASE/etc/profile.d/init.sh
 
-sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/detoxd > /etc/init.d/detoxd
-sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dealerd > /etc/init.d/dealerd
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dynamo-detoxd > /etc/init.d/dynamo-detoxd
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dynamo-dealerd > /etc/init.d/dynamo-dealerd
 
 mkdir -p $DYNAMO_LOGDIR
 chmod 775 $DYNAMO_LOGDIR
