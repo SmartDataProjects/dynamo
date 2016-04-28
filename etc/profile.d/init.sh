c5d3fe68f10eee539860c16fc5ade0c2f7ce3960
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-28 15:25:11 -0400

    adding PATH

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -3,4 +3,6 @@ export DYNAMO_LOGDIR=/var/log/dynamo
 export DYNAMO_DETOX_INTERVAL=6
 export DYNAMO_DEALER_INTERVAL=1
 export X509_USER_PROXY=/tmp/x509up_u$(id -u)
+
+export PATH=$DYNAMO_BASE/bin:$(echo $PATH | sed "s|$DYNAMO_BASE/bin:||")
 export PYTHONPATH=$DYNAMO_BASE/lib:$(echo $PYTHONPATH | sed "s|$DYNAMO_BASE/lib:||")
--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ source $DYNAMO_BASE/etc/profile.d/init.sh
 
 sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dynamo-detoxd > /etc/init.d/dynamo-detoxd
 sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dynamo-dealerd > /etc/init.d/dynamo-dealerd
+chmod +x /etc/init.d/dynamo-detoxd
+chmod +x /etc/init.d/dynamo-dealerd
 
 mkdir -p $DYNAMO_LOGDIR
 chmod 775 $DYNAMO_LOGDIR
