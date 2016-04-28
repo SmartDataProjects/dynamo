9794aa59d9e4bfedab8d7b3524a72b3cd9c1032b
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-28 11:06:21 -0400

    ditching configure scheme

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-DYNAMO_BASE=_DYNAMO_BASE_
-
+export DYNAMO_BASE=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 source $DYNAMO_BASE/etc/profile.d/init.sh
 
-cp $DYNAMO_BASE/sysv/detoxd /etc/init.d/
-cp $DYNAMO_BASE/sysv/dealerd /etc/init.d/
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/detoxd > /etc/init.d/
+sed "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/sysv/dealerd > /etc/init.d/
 
 mkdir -p $DYNAMO_LOGDIR
 chmod 775 $DYNAMO_LOGDIR
 
-crontab $DYNAMO_BASE/etc/crontab
+sed -i "s|_DYNAMO_BASE_|$DYNAMO_BASE|" $DYNAMO_BASE/etc/crontab | crontab
