d71e9472176cd24ee728549b27cca1394296f12a
Yutaro Iiyama <yiiyama@mit.edu>
2016-06-30 11:05:24 -0400

    allow quota to be 0, compute replica size with group

---
+++ lib/common/interface/sitequota.py
@@ -0,0 +1,17 @@
+from common.interface.mysql import MySQL
+
+class SiteQuotaRetriever(object):
+    """
+    A temporary interface to IntelROCCS site quota database.
+    """
+
+    def __init__(self):
+        self._mysql = MySQL(config_file = '/etc/my.cnf', config_group = 'mysql-ddm', db = 'IntelROCCS')
+
+    def get_quota(self, site, group):
+        entry = self._mysql.query('SELECT q.`SizeTb` FROM `Quotas` AS q INNER JOIN `Sites` AS s ON s.`SiteId` = q.`SiteId` INNER JOIN `Groups` AS g ON g.`GroupId` = q.`GroupId` WHERE s.`SiteName` LIKE %s AND g.`GroupName` LIKE %s ORDER BY q.`EntryDateUT` DESC LIMIT 1', site.name, group.name)
+
+        if len(entry) == 0:
+            return 0
+
+        return entry[0]
