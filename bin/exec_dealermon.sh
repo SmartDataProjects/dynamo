11b976bf7db04ec66c10cc289ed03de43829d7f1
Benedikt Maier <benedikt.maier@cern.ch>
2016-08-31 11:48:57 -0400

    Fixing typo in bin/exec_dealermon.sh and making creation of Site overview more intuitive

--- bin/exec_dealermon.sh
+++ bin/exec_dealermon.sh
@@ -3,4 +3,4 @@
 export DYNAMO_BASE=$(dirname $(cd $(dirname ${BASH_SOURCE[0]}); pwd))
 source $DYNAMO_BASE/etc/profile.d/init.sh
 
-$DYNAMO_NASE/bin/track_transfers
+$DYNAMO_BASE/bin/track_transfers
