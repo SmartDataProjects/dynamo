0e5cfbac5405550ea564e9664227118dbdca0c62
Yutaro Iiyama <yiiyama@mit.edu>
2017-05-01 11:55:45 -0400

    reorganizing web to have document root at html/dynamo

--- web/install.sh
+++ web/install.sh
@@ -7,18 +7,21 @@ then
 fi
 
 TARGET=/var/www
-HTMLTARGET=$TARGET/html
+HTMLTARGET=$TARGET/html/dynamo
 BINTARGET=$TARGET/cgi-bin
+HTMLSOURCE=$DYNAMO_BASE/web/html/dynamo
+BINSOURCE=$DYNAMO_BASE/web/cgi-bin
 
 if [ -d $HTMLTARGET ] && [ -d $BINTARGET ]
 then
   mkdir -p $HTMLTARGET/dynamo
-  cp -r $DYNAMO_BASE/web/html/dynamo/* $HTMLTARGET/dynamo/
+  cp -r $HTMLSOURCE/dynamo/* $HTMLTARGET/dynamo/
   mkdir -p $HTMLTARGET/registry
+
   mkdir -p $BINTARGET/dynamo
-  cp -r $DYNAMO_BASE/web/cgi-bin/dynamo/* $BINTARGET/dynamo/
+  cp -r $BINSOURCE/dynamo/* $BINTARGET/dynamo/
   mkdir -p $BINTARGET/registry
-  cp -r $DYNAMO_BASE/web/cgi-bin/registry/* $BINTARGET/registry/
+  cp -r $BINSOURCE/registry/* $BINTARGET/registry/
 
   [ -L $HTMLTARGET/dynamo/detox.php ] || ln -sf $BINTARGET/dynamo/detox/main.php $HTMLTARGET/dynamo/detox.php
   [ -L $HTMLTARGET/dynamo/inventory.php ] || ln -sf $BINTARGET/dynamo/inventory/main.php $HTMLTARGET/dynamo/inventory.php
