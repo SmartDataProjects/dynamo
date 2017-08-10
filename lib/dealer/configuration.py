89127dd8083c9117b2723b778c1df20a97c61e5b
Yutaro Iiyama <yiiyama@mit.edu>
2017-08-10 07:29:10 -0400

    tuning copy volume per cycle down

--- lib/dealer/configuration.py
+++ lib/dealer/configuration.py
@@ -11,7 +11,7 @@ max_dataset_size = 50. # Maximum dataset size to consider for copy in TB
 
 request_to_replica_threshold = 1.75 # (weighted number of requests) / (number of replicas) above which replication happens
 
-max_copy_per_site = 50. # Maximum volume to be copied per site in TB
+max_copy_per_site = 10. # Maximum volume to be copied per site in TB
 max_copy_total = 200.
 
 max_replicas = 10
