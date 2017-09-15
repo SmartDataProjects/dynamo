7b1d8b1334b1808cd7de62655a5341a4e6cb0ef4
Yutaro Iiyama <yiiyama@mit.edu>
2017-09-15 12:46:36 -0400

    we do need the datadir after all

--- etc/profile.d/init.sh
+++ etc/profile.d/init.sh
@@ -1,6 +1,7 @@
 export DYNAMO_BASE=$(dirname $(dirname $(cd $(dirname ${BASH_SOURCE[0]}); pwd)))
 export DYNAMO_ARCHIVE=/mnt/hadoop/$USER/dynamo
 export DYNAMO_SPOOL=/var/spool/dynamo
+export DYNAMO_DATADIR=/local/dynamo/dynamo
 export DYNAMO_LOGDIR=/var/log/dynamo
 export X509_USER_PROXY=/tmp/x509up_u$(id -u)
 
