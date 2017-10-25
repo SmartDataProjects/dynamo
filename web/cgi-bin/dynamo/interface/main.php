f2bb04054fb6cf317cfe19ce4cc59d92bc86e662
Benedikt Maier <benedikt.maier@cern.ch>
2017-10-25 15:19:10 -0400

    tiny steps towards interface

--- web/cgi-bin/dynamo/interface/main.php
+++ web/cgi-bin/dynamo/interface/main.php
@@ -7,13 +7,14 @@ $db_name = 'dynamoregister';
 $db = new mysqli($db_conf['host'], $db_conf['user'], $db_conf['password'], $db_name);
 
 $uploadpath = "/var/www/html/dynamo/dynamo/images/";
-//echo $_REQUEST['filename'];
+$email = $_REQUEST['email'];
+$type = 'gi'; // generic interaction
 $filedata = $_FILES['file']['tmp_name'];
 $filename = $_FILES['file']['name'];
 if ($filedata != ''){
   $hash = hash_file('md5',$filedata);
   copy($filedata,$uploadpath.$hash);
-  communicate($hash,$db);
+  communicate($hash,$db,$email,$type);
 }
 else{
   $html = file_get_contents(__DIR__ . '/html/interface.html');
--- web/cgi-bin/dynamo/interface/upload_file.php
+++ web/cgi-bin/dynamo/interface/upload_file.php
@@ -3,21 +3,33 @@
 include('/var/www/cgi-bin/dynamo/common/db_conf.php');
 include('/var/www/cgi-bin/dynamo/common/communicator.php');
 
-if ($_FILES["file"]["error"] > 0)
+$filename=$_FILES['file']['name'];
+
+if ($_FILES['file']['error'] > 0)
   {
-    echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    echo "Error: " . $_FILES['file']['error'] . "<br>";
+  }
+elseif ( end(explode('.', $filename)) != "txt" )
+  {
+    //$filename = $_FILES['file']['name'];
+    echo "<div style ='font:20px/21px Arial,tahoma,sans-serif;color:#000000'> Error: file $filename does not have the correct type.</div>" . "<br>";
+    echo "<div style ='font:20px/21px Arial,tahoma,sans-serif;color:#000000'> It has to be a .txt file.</div>" . "<br>";
   }
 else
   {
-    echo $_REQUEST['emailAddress'];
+    $db_name = 'dynamoregister';
+    $db = new mysqli($db_conf['host'], $db_conf['user'], $db_conf['password'], $db_name);
+
+    $email=$_POST['email'];
     $uploadpath = '/var/www/html/dynamo/dynamo/images/';
     //$uploadpath = '/var/spool/dynamo/interfaced_actions/';
     $filedata = $_FILES['file']['tmp_name'];
-    $filename = $_FILES['file']['name'];
-    if (copy($filedata,$uploadpath.$filename)){
-      $hash = hash_file('md5',$uploadpath.$filename);
-      communicate($hash,$db);
-      echo "Successfully uploaded. Awaiting results.";
+    //$filename = $_FILES['file']['name'];
+    $hash = hash_file('md5',$filedata);
+    if (copy($filedata,$uploadpath.$hash)){
+      communicate($hash,$db,$email,"dc");
+      echo "<div style ='font:20px/21px Arial,tahoma,sans-serif;color:#000000'> Policy file successfully uploaded. Results will be sent to $email. Be patient.</div>" . "<br>";
+      echo "<div style ='font:14px/21px Arial,tahoma,sans-serif;color:#000000'> Your dynamo team.</div>";
     }
     else{
       echo "Something went wrong.";
