cf2a92b3f8948de18a55df9672e465670646f1a4
Yutaro Iiyama <yiiyama@mit.edu>
2018-01-14 16:47:16 -0500

    standardized logging

--- lib/core/executable.py
+++ lib/core/executable.py
@@ -8,3 +8,26 @@ from dynamo.core.inventory import ObjectRepository
 
 registry = None
 inventory = ObjectRepository()
+
+import sys
+import logging
+
+def make_standard_logger(level):
+    log_level = getattr(logging, level.upper())
+    log_format = '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+    
+    # Everything above log_level goes to stdout
+    out_handler = logging.StreamHandler(sys.stdout)
+    out_handler.setLevel(log_level)
+    out_handler.setFormatter(logging.Formatter(fmt = log_format))
+    # If >= ERROR, goes also to stderr
+    err_handler = logging.StreamHandler(sys.stderr)
+    err_handler.setLevel(logging.ERROR)
+    err_handler.setFormatter(logging.Formatter(fmt = log_format))
+    
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.addHandler(out_handler)
+    logger.addHandler(err_handler)
+
+    return logger
