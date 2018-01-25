f38478b724550c4a4a1501f6f09bc87393e75248
Benedikt Maier <benedikt.maier@cern.ch>
2018-01-25 15:14:47 -0500

    Enforcer

--- lib/policy/producers/enforcerprotected.py
+++ lib/policy/producers/enforcerprotected.py
@@ -1,21 +1,35 @@
+import re
+
+from dynamo.dataformat import Configuration
+
 class EnforcedProtectionTagger(object):
     """
     Checks if the enforcer rules are respected.
     Sets one attr:
-      enforcer_protected:    bool
+      enforcer_protected_replicas:    set of replicas
     """
 
-    produces = ['enforcer_protected']
+    produces = ['enforcer_protected_replicas']
 
     def __init__(self, config):
-        self.sites = list(config.sites)
+        self.policy = Configuration(config.policy)
 
     def load(self, inventory):
-        if len(self.sites) == 0:
-            return
+        for rule in self.policy.rules:        
+            for dataset in inventory.datasets.itervalues():
+                pattern = re.compile(rule['datasets'].replace("*","[^\s]*"))
+                if not pattern.match(dataset.name):
+                    continue
 
-        for dataset in inventory.datasets.itervalues():
-            for replica in dataset.replicas:
-                if replica.site.name in self.sites:
-                    dataset.attr['on_protected_site'] = True
-                    break
+                replicas_in_question = []
+
+                for sitename in rule['sites']:
+                    pattern = re.compile(sitename.replace("*","[^\s]*"))
+                    for site in inventory.sites.values():
+                        if not pattern.match(site.name):
+                            continue
+                        if dataset.find_replica(site) is not None:
+                            replicas_in_question.append(dataset.find_replica(site))
+
+                if len(replicas_in_question) <= rule['copies']:
+                    dataset.attr['enforcer_protected_replicas'] = set(replicas_in_question)
