9ab7cd53c169f1bf6ce5bf573c89d3adb4a89cfa
Yutaro Iiyama <yiiyama@mit.edu>
2018-04-17 15:35:18 -0400

    quota can be negative

--- mysql/schema/dynamo/quotas.sql
+++ mysql/schema/dynamo/quotas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `quotas` (
   `site_id` int(10) unsigned NOT NULL,
   `partition_id` int(10) unsigned NOT NULL,
-  `storage` int(10) unsigned NOT NULL,
+  `storage` int(10) NOT NULL,
   PRIMARY KEY (`site_id`,`partition_id`),
   KEY `partitions` (`partition_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
