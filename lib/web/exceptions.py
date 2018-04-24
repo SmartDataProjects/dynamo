9f98784dd84583f7701df81411bd3f7e0a249a2e
Yutaro Iiyama <yiiyama@mit.edu>
2018-04-24 19:10:48 -0400

    intermediate update

---
+++ lib/web/exceptions.py
@@ -0,0 +1,10 @@
+class MissingParameter(Exception):
+    """Raise if request is missing a required parameter."""
+    def __init__(self, param_name):
+        self.param_name = param_name
+
+class IllFormedRequest(Exception):
+    """Raise if request parameter value does not conform to a format."""
+    def __init__(self, param_name, value):
+        self.param_name = param_name
+        self.value = value
--- lib/web/server.py
+++ lib/web/server.py
@@ -1,26 +1,56 @@
+import sys
 import json
 
-from dynamo.web.get import getters
-from dynamo.web.set import setters
-from dynamo.utils.interface.mysql import MySQL
+import dynamo.web.exceptions as exceptions
 
 class WebServer(object):
+    class DynamoHandle(object):
+        # One-stop object binding all Dynamo objects
+        def __init__(self, server, config):
+            self.inventory = server.inventory
+            self.server_manager = server.manager
+            self.registry = server.registry
+            #self.history = 
+
     def __init__(self, config, dynamoserver):
-        self._dynamo = dynamoserver
-        self._registry = MySQL(config.registry) # will replace with something more abstract
+        self.handle = WebServer.DynamoHandle(dynamoserver, config)
 
     def main(self, environ, start_response):
         name = environ['SCRIPT_NAME'].strip('/')
         command = environ['PATH_INFO'][1:]
+
+        try:
+            if name == 'get':
+                func = getters[command]
+            elif name == 'set':
+                func = setters[command]
+            else:
+                raise KeyError(name)
+        except KeyError:
+            start_response('404 Not Found', [('Content-Type', 'text/plain')])
+            return 'Invalid request %s/%s.' % (name, command)
+
         request = {}
         for query in environ['QUERY_STRING'].split('&'):
             key, _, value = query.partition('=')
             request[key] = value
-        
-        if name == 'get':
-            content = getters[command](request, self._dynamo.inventory, self._registry, self._dynamo.manager.master)
-        elif name == 'set':
-            content = setters[command](request, self._dynamo.inventory, self._registry, self._dynamo.manager.master)
 
-        start_response('200 OK', [('Content-Type', 'text/plain')])
+        try:
+            try:
+                content = func(request, self.handle)
+            except exceptions.MissingParameter as ex:
+                msg = 'Missing required parameter "%s".' % ex.param_name
+                raise
+            except exceptions.IllFormedRequest as ex:
+                msg = 'Parameter "%s" has illegal value "%s".' % (ex.param_name, ex.value)
+                raise
+            except:
+                start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+                return 'Exception: ' + str(sys.exc_info()[1])
+
+        except:
+            start_response('400 Bad Request', [('Content-Type', 'text/plain')])
+            return msg
+
+        start_response('200 OK', [('Content-Type', 'application/json')])
         return json.dumps(content)
