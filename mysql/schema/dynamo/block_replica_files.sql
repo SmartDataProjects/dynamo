dd023d4f035ceb875dfbd857112fcf9e60c3a1df
Yutaro Iiyama <yiiyama@mit.edu>
2018-05-25 19:12:38 -0400

    forgot to add files sql

---
+++ mysql/schema/dynamo/block_replica_files.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `block_replica_files` (
+  `block_id` bigint(20) unsigned NOT NULL,
+  `site_id` int(10) unsigned NOT NULL,
+  `file_id` bigint(20) NOT NULL,
+  UNIQUE KEY `filereplica` (`file_id`,`site_id`),
+  KEY `blockreplica` (`block_id`, `site_id`),
+  KEY `site` (`site_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 CHECKSUM=1;
