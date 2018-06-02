35b98e8cca43772b6e45b253357b4d0f85b3ea86
Yutaro Iiyama <yiiyama@mit.edu>
2018-06-01 21:53:01 -0400

    moved tables to dynamo DB

--- mysql/schema/dynamo/deletion_queue.sql
+++ mysql/schema/dynamo/deletion_queue.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `deletion_queue` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `desubscription_id` bigint(20) unsigned NOT NULL,
+  `subscription_id` bigint(20) unsigned NOT NULL,
   `batch_id` bigint(20) unsigned NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `desubscription` (`desubscription_id`),
+  UNIQUE KEY `subscription` (`subscription_id`),
   KEY `batch` (`batch_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
