e1c4e239da083a813494450b3cd16d1408da4935
Yutaro Iiyama <yiiyama@mit.edu>
2018-06-02 14:55:13 -0400

    minor tweaks

--- mysql/schema/dynamo/dataset_replicas.sql
+++ mysql/schema/dynamo/dataset_replicas.sql
@@ -2,7 +2,7 @@ CREATE TABLE `dataset_replicas` (
   `dataset_id` int(11) unsigned NOT NULL,
   `site_id` int(11) unsigned NOT NULL,
   `growing` tinyint(1) unsigned NOT NULL,
-  `group_id` int(11) unsigned NULL,
+  `group_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`dataset_id`,`site_id`),
   KEY `sites` (`site_id`),
   KEY `groups` (`group_id`)
