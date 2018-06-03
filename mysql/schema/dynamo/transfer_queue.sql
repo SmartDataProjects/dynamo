04b5de5ae779f42c4d6a1140be4ae6f448ba2950
Yutaro Iiyama <yiiyama@mit.edu>
2018-06-03 17:47:02 -0400

    fileop

--- mysql/schema/dynamo/transfer_queue.sql
+++ mysql/schema/dynamo/transfer_queue.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `transfer_queue` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `subscription_id` bigint(20) unsigned NOT NULL,
-  `source` int(11) unsigned NOT NULL,
+  `source_id` int(11) unsigned NOT NULL,
   `batch_id` bigint(20) unsigned NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `transfer` (`subscription_id`),
-  KEY `source` (`source`),
+  KEY `source` (`source_id`),
   KEY `batch` (`batch_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
