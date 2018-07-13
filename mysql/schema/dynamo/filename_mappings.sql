b9ec5b948dcd49d515cddc34caceff958b12c2f1
Yutaro Iiyama <yiiyama@mit.edu>
2018-07-13 13:49:55 -0400

    fix

--- mysql/schema/dynamo/filename_mappings.sql
+++ mysql/schema/dynamo/filename_mappings.sql
@@ -5,5 +5,5 @@ CREATE TABLE `filename_mappings` (
   `index` int(11) unsigned NOT NULL,
   `lfn_pattern` varchar(512) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
   `pfn_pattern` varchar(512) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  KEY (`site_id`, `protocol`, `chain_id`, `index`),
+  KEY (`site_id`, `protocol`, `chain_id`, `index`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 CHECKSUM=1;
