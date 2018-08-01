54787623eddbaa4c2e2821203adc44b9f6c90676
Yutaro Iiyama <yiiyama@mit.edu>
2018-08-01 14:11:22 -0400

    check in

--- lib/web/modules/transfers/__init__.py
+++ lib/web/modules/transfers/__init__.py
@@ -1,10 +1,12 @@
 import current
 import history
 import monitor
+import held
 
 export_data = {}
 export_data.update(current.export_data)
 export_data.update(history.export_data)
+export_data.update(held.export_data)
 
 export_web = {}
 export_web.update(monitor.export_web)
