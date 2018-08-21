6cfefffd8cbcb91031b302576eb92bddb848682b
Christoph Paus <paus@mit.edu>
2018-08-21 11:36:29 -0400

    Rolling back to previous version.

--- mysql/schema/dynamo/software_versions.sql
+++ mysql/schema/dynamo/software_versions.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `software_versions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `config` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `version` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `version` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `version` (`config`,`version`)
+  UNIQUE KEY `version` (`version`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 CHECKSUM=1;
