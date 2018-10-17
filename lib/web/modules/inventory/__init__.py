ff0c5ee5561b2bb87a2bb73594624ad3f915de3b
maksiks <maxi@mit.edu>
2018-10-17 11:21:34 -0400

    adding new modules to init

--- lib/web/modules/inventory/__init__.py
+++ lib/web/modules/inventory/__init__.py
@@ -1,17 +1,24 @@
 import datasets
 import groups
-import sites
 import stats
 import inject
 import delete
+import blockreplicas, requestlist, subscriptions, lfn2pfn, nodes, data, transferrequests
 
 export_data = {}
 export_data.update(datasets.export_data)
 export_data.update(groups.export_data)
-export_data.update(sites.export_data)
 export_data.update(stats.export_data)
 export_data.update(inject.export_data)
 export_data.update(delete.export_data)
+export_data.update(blockreplicas.export_data)
+export_data.update(requestlist.export_data)
+export_data.update(subscriptions.export_data)
+export_data.update(lfn2pfn.export_data)
+export_data.update(nodes.export_data)
+export_data.update(data.export_data)
+export_data.update(transferrequests.export_data)
+
 
 export_web = {}
 export_web.update(stats.export_web)
