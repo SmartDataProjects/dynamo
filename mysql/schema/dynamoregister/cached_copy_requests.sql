cf3c9921ce9ab4c75fa76b91065b96ae34ad82e3
Dynamo <dynamo@dynamo.mit.edu>
2019-03-12 10:38:08 -0400

    caching db schema

---
+++ mysql/schema/dynamoregister/cached_copy_requests.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE `cached_copy_requests` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sites` varchar(64) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+  `item` varchar(256) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+  `group` varchar(32) NOT NULL,
+  `num_copies` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user` varchar(64) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `dn` varchar(256) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+  `request_time` datetime NOT NULL,
+  `status` enum('new','activated') NOT NULL DEFAULT 'new',
+  PRIMARY KEY (`id`),
+  KEY `request_time` (`request_time`),
+  KEY `status` (`status`),
+  KEY `user` (`user`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
